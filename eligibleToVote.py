b6dedbdac1bd75cd1d9036a6483ae294ff669a32
ananyaR0308 <ananyasrivastva51@gmail.com>
2025-07-17 19:23:11 +0530

    Vote age check

---
+++ eligibleToVote.py
@@ -0,0 +1,12 @@
+import time as t
+print("Hello Sir!!!")
+t.sleep(1)
+print("Check Your Eligiblity For Vote!!")
+t.sleep(1)
+print("Enter Your Age Here ")
+t.sleep(1)
+Age=int(input("Enter:- "))
+if (Age>=18):
+   print("You are Eligible!!")
+else:
+   print("You are not Eligible")
